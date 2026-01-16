21995a3b5c11cbbeeaf03cf71da6f1897ff62bed
Puustinen Petri <petri.puustinen@etteplan.com>
2026-01-16 09:51:51 +0200

    changed base image

--- 1_start.bat
+++ 1_start.bat
@@ -1,9 +1,8 @@
-
 set usr=postgres
 set pwd=postgres
 set pgd=/var/lib/postgresql/data/pgdata
-set vol=C:\Users\xxx\postgres-test\data:/var/lib/postgresql/data
-set img=postgres:14.1-alpine
+set vol=<usr>\postgres-test\:/var/lib/postgresql/data
+set img=postgres:16.11-alpine
 set port=5432:5432
 set name=basic-postgres
 
--- 3_insert.bat
+++ 3_insert.bat
@@ -1,12 +1,16 @@
+set usr=postgres
+set dmp=xxx.dump
 
 :: docker cp ./test_data_1.json basic-postgres:test_data_1.json && ( pause ) || ( pause )
 :: docker cp ./test_data_1.csv basic-postgres:test_data_1.csv && ( pause ) || ( pause )
 :: docker exec -it basic-postgres ls -l && ( pause ) || ( pause )
-:: docker exec -it basic-postgres psql -U postgres -d postgres  feeds -c "SELECT * FROM PERSON;"  && ( pause ) || ( pause )
+:: docker exec -it basic-postgres psql -U %usr% -d %usr%  feeds -c "SELECT * FROM PERSON;"  && ( pause ) || ( pause )
 
-:: docker exec -it basic-postgres psql -U postgres -d postgres feeds -c  "COPY PERSON (PERSON_KEY, NAME, ATTRIBUTES, CREATED, STATUS) From './test_data_1.csv' DELIMITER ',' CSV HEADER;" && ( pause ) || ( pause )
-:: docker exec -it basic-postgres psql -U postgres -d postgres \copy person FROM PROGRAM 'jq -c -r .[] < /root/test_data_1.json'; && ( pause ) || ( pause )
+:: docker exec -it basic-postgres psql -U %usr% -d %usr% feeds -c  "COPY PERSON (PERSON_KEY, NAME, ATTRIBUTES, CREATED, STATUS) From './test_data_1.csv' DELIMITER ',' CSV HEADER;" && ( pause ) || ( pause )
+:: docker exec -it basic-postgres psql -U %usr% -d %usr% \copy person FROM PROGRAM 'jq -c -r .[] < /root/test_data_1.json'; && ( pause ) || ( pause )
 
+:: docker cp %dmp% basic-postgres:%dmp% && ( pause ) || ( pause )
+:: docker exec -it basic-postgres pg_restore -h localhost -d %usr% -U %usr% %dmp% && ( pause ) || ( pause )
 
 
 
